74041d004a3626808d4ed0d247e46ae121ef2474
Ppatel122 <pranj1012@gmail.com>
2024-02-19 15:10:57 -0700

    Finished Story 42

--- mysocial/admin.py
+++ mysocial/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.auth.models import User
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Author)
+
+def make_active(modeladmin, request, queryset):
+    queryset.update(is_active=True)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'is_active']
+    ordering = ['username']
+    actions = [make_active]
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
