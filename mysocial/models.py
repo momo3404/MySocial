6f08fb828fe33c6f265c0a76322098915c55787e
Ppatel122 <pranj1012@gmail.com>
2024-02-24 15:03:47 -0700

    Fixed Author API

--- mysocial/models.py
+++ mysocial/models.py
@@ -2,31 +2,34 @@ from django.db import models
 from django.contrib.auth.models import User
 import uuid
 
+#REMOVE THESE AFTER REFACTOR
 MAX_LENGTH = 1000
 SHORT_MAX_LENGTH = 200
 
-# Create your models here.
+#KEEP THESE
+SHORT_LENGTH = 30 
+ID_LENGTH = 500 
+URL_LENGTH = 1000
     
 class Author(models.Model):
-    type = models.CharField(max_length=30, null=True)
-    url_id = models.URLField(max_length=MAX_LENGTH, unique=True, null=False)
-    author_id = models.UUIDField(max_length=MAX_LENGTH, unique=True, null=True, blank=True)
-    display_name = models.CharField(max_length=MAX_LENGTH, null=True)
-    host = models.URLField(max_length=MAX_LENGTH, null=True)
-    github = models.URLField(max_length=MAX_LENGTH, blank=True, null=True)
-    profile_url = models.URLField(max_length=MAX_LENGTH,null=True) 
-    profile_image = models.URLField(max_length=MAX_LENGTH, null=True)
+    type = models.CharField(max_length=SHORT_LENGTH, null=True)
+    authorId = models.UUIDField(max_length=ID_LENGTH, unique=True, null=True, blank=True)
+    url = models.URLField(max_length=URL_LENGTH, unique=True, null=False)
+    host = models.URLField(max_length=URL_LENGTH, null=True)
+    displayName = models.CharField(max_length=SHORT_LENGTH, null=True)
+    github = models.URLField(max_length=URL_LENGTH, blank=True, null=True)
+    profileImage = models.URLField(max_length=URL_LENGTH, null=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='author', null=True, blank=False) 
 
     def __str__(self):
-        return self.display_name
+        return self.displayName
     
 class Post(models.Model):
     text = models.CharField(max_length=SHORT_MAX_LENGTH)
     pub_date = models.DateTimeField("date published")
     content = models.TextField(max_length=MAX_LENGTH, null=True, blank=True)
     # foreign key to author
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, to_field='author_id', related_name='posts', null=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, to_field='authorId', related_name='posts', null=True)
 
     # null = True means empty values for the field will be set as NULL
     # blank = True means the field will not be required
--- mysocial/serializers.py
+++ mysocial/serializers.py
@@ -2,20 +2,19 @@ from rest_framework import serializers
 from .models import *
 
 class AuthorSerializer(serializers.Serializer):
-    class Meta:
-        model = Author
-        fields = ['type', 'url_id', 'author_id', 'display_name', 'host', 'github', 'profile_url', 'profile_image']
-        
     type = serializers.CharField(max_length=30)
-    url_id = serializers.URLField(max_length=200)  
-    author_id = serializers.UUIDField(format='hex_verbose', required=False, allow_null=True)  
-    display_name = serializers.CharField(max_length=200)  
+    authorId = serializers.UUIDField(format='hex_verbose', required=False, allow_null=True)  
+    url = serializers.URLField(max_length=200)  
     host = serializers.URLField(max_length=200)  
+    displayName = serializers.CharField(max_length=200)  
     github = serializers.URLField(max_length=200, allow_blank=True, allow_null=True)  
-    profile_url = serializers.URLField(max_length=200)  
-    profile_image = serializers.URLField(max_length=200)  
+    profileImage = serializers.URLField(max_length=200)  
     user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), allow_null=True)
 
+    class Meta:
+        model = Author
+        fields = ['type', 'authorId', 'url', 'host', 'displayName', 'github', 'profileImage']
+        
     def create(self, validated_data):
         """
         Create and return a new `author` instance, given the validated data
@@ -27,13 +26,12 @@ class AuthorSerializer(serializers.Serializer):
         Update and return an existing `author` instance, given the validated data
         """
         instance.type = validated_data.get('type', instance.type)
-        instance.url_id = validated_data.get('url_id', instance.url_id)
-        instance.author_id = validated_data.get('author_id', instance.author_id)
-        instance.display_name = validated_data.get('display_name', instance.display_name)
+        instance.authorId = validated_data.get('authorId', instance.authorId)
+        instance.url = validated_data.get('url', instance.url)
         instance.host = validated_data.get('host', instance.host)
+        instance.displayName = validated_data.get('displayName', instance.displayName)
         instance.github = validated_data.get('github', instance.github)
-        instance.profile_url = validated_data.get('profile_url', instance.profile_url)
-        instance.profile_image = validated_data.get('profile_image', instance.profile_image)
+        instance.profileImage = validated_data.get('profileImage', instance.profileImage)
         instance.save()
         return instance
     
