0804ce45f5bcc256c7d907ac4dcccb7c3a8db0ae
Muhanned Al-Bachachi <momo.albachachi@gmail.com>
2024-02-23 21:16:30 -0700

    fixed views.py errors

--- mysocial/views.py
+++ mysocial/views.py
@@ -63,9 +63,9 @@ def connect_to_remote_server(remote_server_id):
 def public_profile(request, author_id):
     try:
         # author_id string from URL to a UUID object
-        author_uuid = uuid.UUID(author_id)
+        author_uuid = uuid.UUID(str(author_id))
         author = get_object_or_404(Author, author_id=author_uuid)
-        return render(request, 'profile/public_profile.html', {'author': author})
+        return render(request, 'mysocial/public_profile.html', {'author': author})
     except ValueError:
         # if ID not a valid UUID
         raise Http404("Invalid Author ID")
