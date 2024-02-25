74e67cf570ec82d44ad4da95300332d0a75dc54b
Ppatel122 <pranj1012@gmail.com>
2024-02-24 18:35:25 -0700

    Added models

--- mysocial/models.py
+++ mysocial/models.py
@@ -8,8 +8,10 @@ SHORT_MAX_LENGTH = 200
 
 #KEEP THESE
 SHORT_LENGTH = 30 
+MEDIUM_LENGTH = 100
 ID_LENGTH = 500 
 URL_LENGTH = 1000
+CONTENT_LENGTH = 2000
     
 class Author(models.Model):
     type = models.CharField(max_length=SHORT_LENGTH, null=True)
@@ -23,24 +25,55 @@ class Author(models.Model):
 
     def __str__(self):
         return self.displayName
+
+class Follower(models.Model):
+    attributes = models.JSONField(null=True, blank=True)
+    follower = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='items', null=True, blank=True)
+    
+    def __str__(self):
+        return self.follower.displayName + "  sent a follow request to  " + self.attributes["displayName"]
+
+class FollowRequest(models.Model):
+    type = models.CharField(max_length=SHORT_LENGTH, default="Follow")
+    summary = models.CharField(max_length=CONTENT_LENGTH, null=True)
+    actor = models.JSONField()
+    object = models.JSONField()
+    
+    def __str__(self):
+        return self.summary
     
 class Post(models.Model):
-    text = models.CharField(max_length=SHORT_MAX_LENGTH)
-    pub_date = models.DateTimeField("date published")
-    content = models.TextField(max_length=MAX_LENGTH, null=True, blank=True)
-    # foreign key to author
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, to_field='authorId', related_name='posts', null=True)
-
-    # null = True means empty values for the field will be set as NULL
-    # blank = True means the field will not be required
-
-    title =  models.CharField(max_length=MAX_LENGTH, null=True)
-    url_id = models.URLField(max_length=MAX_LENGTH, unique=True, null=True)
-    post_id = models.UUIDField(max_length=MAX_LENGTH, unique=True, null=True)
-    description = models.TextField(max_length=MAX_LENGTH, null=True, blank=True)
+    type = models.CharField(max_length=SHORT_LENGTH, default="post")
+    title =  models.CharField(max_length=MEDIUM_LENGTH, null=True)
+    postId = models.UUIDField(max_length=ID_LENGTH, unique=True, null=True, blank=True)
+    url = models.URLField(max_length=URL_LENGTH, unique=True, null=True)
+    source = models.URLField(max_length=URL_LENGTH, null=True)
+    origin =  models.URLField(max_length=URL_LENGTH, null=True)
+    description = models.TextField(max_length=CONTENT_LENGTH, null=True, blank=True)
+    content_type = models.CharField(max_length=SHORT_LENGTH, null=True)
+    content = models.TextField(max_length=CONTENT_LENGTH, null=True, blank=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='author', null=True, blank=True)
+    count = models.IntegerField(null=True)
+    comments = models.URLField(max_length=URL_LENGTH, null=True)
+    published = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    visibility = models.CharField(max_length=SHORT_LENGTH, default="PUBLIC", null=False)
 
     def __str__(self):
-        return self.text
+        return self.title
+
+class Comment(models.Model):
+    type = models.CharField(max_length=SHORT_LENGTH, default="comment")
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='comment_author')
+    comment = models.TextField(max_length=CONTENT_LENGTH, null=False)
+    contentType = models.CharField(max_length=SHORT_LENGTH, null=False)
+    published = models.DateTimeField(auto_now_add=True)
+    commentId = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comment_post')
+
+    def __str__(self):
+        return self.author.displayName + " commented"
+   
+    
     
 class Node(models.Model):
     node_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
