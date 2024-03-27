c7850a84c10ef365c1ee255460079e616c2ccbb0
triaksh25 <100975695+triaksh25@users.noreply.github.com>
2024-03-26 23:42:56 -0600

    Remote for wlp

--- mysocial/views.py
+++ mysocial/views.py
@@ -1038,7 +1038,15 @@ def send_remote_follow(request):
         data = {
             "type": "Follow",
             "summary": f"{request.user.username} wants to follow {request.POST.get('object_displayName')}",
-            "actor": author_serializer.data,
+            "actor": {
+                "type": "author",
+                "id": int(request.user.author.authorId),
+                "host": request.user.author.host,
+                "displayName": request.user.author.displayName,
+                "url": request.user.author.url,
+                "github": request.user.author.github,
+                #"profileImage": request.user.author.profileImage,
+            },
             "object": {
                 "type": "author",
                 "id": object_id,
@@ -1051,7 +1059,7 @@ def send_remote_follow(request):
         }
         node = Node.objects.get(node_id=node_id)
         
-        response = requests.post(f"{object_id}/inbox/", json=data, auth=HTTPBasicAuth(node.username, node.password))
+        response = requests.post(f"{object_id}/inbox", json=data, auth=HTTPBasicAuth(node.username, node.password))
         
         if response.status_code == 201:
             return JsonResponse({'message': 'Follow request sent successfully.'}, status=201)
